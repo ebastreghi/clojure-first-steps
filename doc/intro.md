92164c13513b3ef227655024675aaa1a6563c2f0
Edevar Bastreghi Filho <edevar-filho@edevar.filho>
2021-03-17 17:55:15 -0300

    Initial project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-first-steps
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_first_steps/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-first-steps.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-first-steps.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
