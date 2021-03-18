c27ed5dfb7b8ef19b3773ea67e2ec9c28775f56b
Edevar Bastreghi Filho <edevar-filho@edevar.filho>
2021-03-18 11:31:25 -0300

    Small challenge - pets age in humans age

---
+++ src/clojure_first_steps/petAge.clj
@@ -0,0 +1,23 @@
+(ns clojure-first-steps.petAge)
+
+(defn petToHumanAge
+  "This function returns the age of the pet is human years"
+  [mapKey]
+  (def petMap {'dog 7, 'cat 5, 'goldfish 10})
+  (get petMap mapKey)
+  )
+
+;return 7
+;(petToHumanAge 'dog)
+
+(defn age
+  "This function returns the age of a pet"
+  [petName petType petAge]
+  ;immutable variable ratio is initialized
+  (def ratio (petToHumanAge petType))
+  (println petName "is" (* ratio petAge) "years old in human years")
+  )
+
+(age "Fido" 'dog 4)
+(age "Fifi" 'cat 2)
+(age "Bubbles" 'goldfish 10)
