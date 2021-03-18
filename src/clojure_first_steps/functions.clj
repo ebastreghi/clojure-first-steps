e97163f19b310bf0cd3ff369f45cce70359687d1
Edevar Bastreghi Filho <edevar-filho@edevar.filho>
2021-03-18 09:40:21 -0300

    Data types

--- src/clojure_first_steps/functions.clj
+++ src/clojure_first_steps/functions.clj
@@ -21,3 +21,24 @@
   [x]
   (map increment x)
   )
+
+(defn DataTypes
+  []
+  (def a 1)
+  (def b 1.25)
+  (def c 1.25e-12)
+  (def d 0xfbfc)
+  (def e nil)
+  (def f true)
+  (def g "Hello")
+  ;keyword
+  (def h 'thanks)
+
+
+  (println a)
+  (println b)
+  (println c)
+  (println d)
+  )
+;call the function
+(DataTypes)
