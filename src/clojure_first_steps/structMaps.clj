aa835dd8ac6578606c5c3140afe3bb39c6d0be83
Edevar Bastreghi Filho <edevar-filho@edevar.filho>
2021-03-19 11:36:22 -0300

    structMaps

---
+++ src/clojure_first_steps/structMaps.clj
@@ -0,0 +1,37 @@
+(ns clojure-first-steps.structMaps)
+
+(defn pets
+  []
+  (defstruct pet :PetType :PetName)
+  (def myPet (struct pet "dog" "Fido"))
+  (println myPet)
+
+  (def myOtherPet (struct-map pet :PetName "Fifi" :PetType "cat"))
+  (println myOtherPet)
+
+  (println (:PetName myPet))
+  (println (:PetType myOtherPet))
+
+  ;assoc change the field of the struct
+  (def myNewPet (assoc myPet :PetName "Max"))
+  (println myNewPet)
+  ;you can ad a new field too
+  (def myNewOtherPet (assoc myOtherPet :PetAge 10))
+  (println myNewOtherPet)
+  )
+(pets
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  )
